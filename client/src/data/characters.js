76657ef5313f0fdd9b19e857461034d1d9f38579
Shane Cook <sc897@nau.edu>
2019-10-30 01:38:05 -0700

    character updates.

--- client/src/data/characters.js
+++ client/src/data/characters.js
@@ -2,10 +2,21 @@ import Jack from '../assets/Jack.svg';
 import OogieBoogie from '../assets/OogieBoogie.svg';
 import Sally from '../assets/Sally.svg';
 import Zero from '../assets/Zero.png';
+import Barrel from '../assets/barrel.png';
+import Lock from '../assets/lock.png';
+import Shock from '../assets/shock.png';
+import Mayor from '../assets/mayor.png';
+import Finkelstein from '../assets/dr_finkelstein.png';
+
 
 export const CHARACTER_LIST = [
   { id: 1, name: 'Jack', img: Jack },
   { id: 2, name: 'Sally', img: Sally },
   { id: 3, name: 'Oogie', img: OogieBoogie },
-  { id: 4, name: 'Zero', img: Zero }
+  { id: 4, name: 'Barrel', img: Barrel },
+  { id: 5, name: 'Lock', img: Lock },
+  { id: 6, name: 'Shock', img: Shock },
+  { id: 7, name: 'Mayor', img: Mayor },
+  { id: 8, name: 'Dr. Finkelstein', img: Finkelstein },
+  { id: 9, name: 'Zero', img: Zero }
 ];
